95bd32d70213c8e2be941143aacfc3cec1d4873f
tswicegood <tswicegood@159b4bd8-5ae1-4aa7-97ff-061b2ff7a14b>
2007-08-23 19:14:25 +0000

    Add docblock comments
    git-svn-id: svn://localhost/Phing_d51PearPkg2Task/trunk@334 159b4bd8-5ae1-4aa7-97ff-061b2ff7a14b

--- src/phing/tasks/ext/d51PearPkg2Task/Release.php
+++ src/phing/tasks/ext/d51PearPkg2Task/Release.php
@@ -1,15 +1,46 @@
 <?php
-
+/**
+ * This file contains {@link d51PearPkg2Task_Release}
+ * 
+ * 
+ * @license http://www.gnu.org/licenses/lgpl.html LGPL
+ * 
+ * @package phing.tasks.ext
+ * 
+ * @subpackage d51pearkpkg2
+ * 
+ */
+/**
+ * Load required classes
+ * @ignore
+ */
 require_once 'phing/tasks/ext/d51PearPkg2Task/Release/Install.php';
 
+/**
+ * Object to handle <release> elements
+ *
+ * @see d51PaerPkg2Task::createRelease()
+ */
 class d51PearPkg2Task_Release
 {
     private $_install = array();
+    
+    /**
+     * Handle instantiation
+     * 
+     * @ignore
+     */
     public function __construct()
     {
         
     }
     
+    /**
+     * Creates a {@link d51PearPkg2Task_Release_Install} object to
+     * handle <install> elements
+     *
+     * @return d51PearPkg2Task_Release_Install
+     */
     public function createInstall()
     {
         $install = new d51PearPkg2Task_Release_Install();
@@ -17,6 +48,14 @@ class d51PearPkg2Task_Release
         return $install;
     }
     
+    
+    /**
+     * Magic method to expose following properties as read-only:
+     * 
+     * install: Contains an array of <install> elements
+     *
+     * @return mixed
+     */
     public function __get($key)
     {
         switch ($key) {
